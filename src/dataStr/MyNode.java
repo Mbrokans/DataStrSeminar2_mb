56c79a61a1b9405bb7b8f434dd15d31f8f7aa5a7
Mbrokans <s25brokmart@venta.lv>
2026-03-20 18:33:37 +0200

    EDIT: added getters and setters

--- src/dataStr/MyNode.java
+++ src/dataStr/MyNode.java
@@ -1,5 +1,36 @@
 package dataStr;
 
 public class MyNode {
-
+	
+	private char element;
+	private MyNode nextNode = null;
+	private MyNode previousNode = null;
+	
+	
+	
+	
+	public char getElement() {
+		return element;
+	}
+	public MyNode getNextNode() {
+		return nextNode;
+	}
+	public MyNode getPreviousNode() {
+		return previousNode;
+	}
+	public void setElement(char element) {
+		this.element = element;
+	}
+	
+	public void setNextNode(MyNode nextNode) {
+		this.nextNode = nextNode;
+	}
+	
+	public void setPreviousNode(MyNode previousNode) {
+		this.previousNode = previousNode;
+	}
+	
+	
+	
+	
 }
